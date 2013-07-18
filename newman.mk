9c30a235b4e2ce4a78d869f116dc6c761adbea4a
Scorpio92 <scorpio92@scorpio92-desktop.(none)>
2013-07-18 23:42:01 +1100

    камера ньюмана и монтирование КП

--- newman.mk
+++ newman.mk
@@ -6,7 +6,10 @@ LOCAL_PATH := vendor/samsung/i9300
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/newman/camera.smdk4x12.so:system/lib/hw/camera.smdk4x12.so \
     $(LOCAL_PATH)/proprietary/newman/libcamera_client.so:system/lib/libcamera_client.so \
-    $(LOCAL_PATH)/proprietary/newman/file_copy.sh:system/etc/file_copy.sh 
+    $(LOCAL_PATH)/proprietary/newman/libfimg.so:system/lib/libfimg.so \
+    $(LOCAL_PATH)/proprietary/newman/libhwjpeg.so:system/lib/libhwjpeg.so \
+    $(LOCAL_PATH)/proprietary/newman/file_copy.sh:system/etc/file_copy.sh \
+    $(LOCAL_PATH)/proprietary/newman/vold.fstab:system/etc/vold.fstab 
 
 $(call inherit-product, $(LOCAL_PATH)/main.mk)
 
