a1839b55a4a0d5e9173a3bf32ae13afdb364db42
Scorpio92 <scorpio92@scorpio92-desktop.(none)>
2013-07-18 23:40:09 +1100

    камера для хайскрина

--- highscreen.mk
+++ highscreen.mk
@@ -6,7 +6,11 @@ LOCAL_PATH := vendor/samsung/i9300
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/highscreen/camera.smdk4x12.so:system/lib/hw/camera.smdk4x12.so \
     $(LOCAL_PATH)/proprietary/highscreen/libcamera_client.so:system/lib/libcamera_client.so \
+    $(LOCAL_PATH)/proprietary/highscreen/libfimg.so:system/lib/libfimg.so \
+    $(LOCAL_PATH)/proprietary/highscreen/libhwjpeg.so:system/lib/libhwjpeg.so \
+    $(LOCAL_PATH)/proprietary/highscreen/libswscaler.so:system/lib/libswscaler.so \
     $(LOCAL_PATH)/proprietary/highscreen/file_copy.sh:system/etc/file_copy.sh \
-    $(LOCAL_PATH)/proprietary/highscreen/vrm_result:system/etc/fota/vrm_result
+    $(LOCAL_PATH)/proprietary/highscreen/vrm_result:system/etc/fota/vrm_result \
+    $(LOCAL_PATH)/proprietary/highscreen/vold.fstab:system/etc/vold.fstab 
 
 $(call inherit-product, $(LOCAL_PATH)/main.mk)
